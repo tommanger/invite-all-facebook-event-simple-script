c30100949a8722fe66ed70a93e63f737d379d93b
Tom Manger <44051908+tommanger@users.noreply.github.com>
2024-02-03 12:02:30 +0200

    change url

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var tags = document.getElementsByTagName('i')
-let url = 'url("https://static.xx.fbcdn.net/rsrc.php/v3/yR/r/TdscYKD_0J7.png")'
+let url = 'url("https://static.xx.fbcdn.net/rsrc.php/v3/yC/r/Y1gksc18stA.png")'
 
 for (var i = 0, len = tags.length; i < len; i++) {
     let biUrl = document.defaultView.getComputedStyle(tags[i], null).getPropertyValue('background-image');
