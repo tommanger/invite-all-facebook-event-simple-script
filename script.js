c894f422b871047ea38601e7730a5ed37a1b68e0
Tom Manger <44051908+tommanger@users.noreply.github.com>
2024-09-05 09:42:41 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var tags = document.getElementsByTagName('i')
-let url = 'url("https://static.xx.fbcdn.net/rsrc.php/v3/yT/r/uc1IduXdrzA.png")'
+let url = 'url("https://static.xx.fbcdn.net/rsrc.php/v3/yh/r/DkJXGpXdSuw.png")'
 
 for (var i = 0, len = tags.length; i < len; i++) {
     let biUrl = document.defaultView.getComputedStyle(tags[i], null).getPropertyValue('background-image');
